b207b2bad2a5abd718a965dbff4d212b58760e8f
DamonBenson <a1696179007@163.com>
2023-01-09 16:49:29 +0800

    TestGit

--- MidBackend.js
+++ MidBackend.js
@@ -165,11 +165,11 @@ async function UseMysql_c2(req, res, handle, params=null) {
 
 }
 /*----------信息查询请求路由配置----------*/
-const authRouter = express.Router({
+const confirmRouter = express.Router({
     caseSensitive: false,// 不区分大小写
 });
 
-const listenRouter = express.Router({
+const surveilanceRouter = express.Router({
     caseSensitive: false,// 不区分大小写
 });
 
@@ -177,87 +177,135 @@ const testRouter = express.Router({
     caseSensitive: false,// 不区分大小写
 });
 
-// 新增存证数量/月
-authRouter.get('/certificateAmountEXchange', async function(req, res) {
+// 不同作品类型存证的时间分布/季
+confirmRouter.get('/certificateAmountGroupByWorkTypeEXchange', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByWorkTypeEXchange);
+});
+// localhost:9181/backend/confirmation/certificateAmountGroupByWorkTypeEXchange
+
+// 作品的时间分布/月
+confirmRouter.get('/certificateAmountEXchange', async function(req, res) {
     await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountEXchange);
 });
-// localhost:9181/backend/certificateAmountEXchange
+// localhost:9181/backend/confirmation/certificateAmountEXchange
 
-// 不同作品类型的存证分布分布
-authRouter.get('/certificateAmountGroupByWorkType', async function(req, res) {
+// 不同作品类型的存证分布
+confirmRouter.get('/certificateAmountGroupByWorkType', async function(req, res) {
     await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByWorkType);
 });
-// localhost:9181/backend/certificateAmountGroupByWorkType
+// localhost:9181/backend/confirmation/certificateAmountGroupByWorkType
 
-// 新增不同作品类型的存证数量/季
-authRouter.get('/certificateAmountGroupByWorkTypeEXchange', async function(req, res) {
+// 不同著作权产生方式的存证分布
+confirmRouter.get('/certificateAmountGroupByCreateType', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByCreateType);
+});
+// localhost:9181/backend/confirmation/certificateAmountGroupByCreateType
+
+// TODO 合作企业的存证分布
+confirmRouter.get('/certificateAmountGroupByCooperator', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByCooperator);
+});
+// localhost:9181/backend/confirmation/certificateAmountGroupByCooperator
+
+// TODO 存证总数
+confirmRouter.get('/certificateCount', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateCount);
+});
+// localhost:9181/backend/confirmation/certificateCount
+
+
+/**************************/
+/****       作品通证     ****/
+/**************************/
+// 不同作品类型通证的时间分布/季
+confirmRouter.get('/certificateAmountGroupByWorkTypeEXchange', async function(req, res) {
     await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByWorkTypeEXchange);
 });
 // localhost:9181/backend/certificateAmountGroupByWorkTypeEXchange
 
-// 新增版权通证数量/月
-authRouter.get('/copyRightAmountEXchange', async function(req, res) {
+// TODO 通证的时间分布/月
+confirmRouter.get('/copyRightAmountEXchange', async function(req, res) {
     await UseMysql_c1(req, res, authDisplayGroup.handleCopyRightAmountEXchange);
 });
 // localhost:9181/backend/copyRightAmountEXchange
 
-// 不同著作权产生方式的存证分布
-authRouter.get('/certificateAmountGroupByCreateType', async function(req, res) {
+// TODO 不同作品类型的通证分布
+confirmRouter.get('/certificateAmountGroupByWorkType', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByWorkType);
+});
+// localhost:9181/backend/certificateAmountGroupByWorkType
+
+// TODO 不同著作权产生方式的通证分布
+confirmRouter.get('/certificateAmountGroupByCreateType', async function(req, res) {
     await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByCreateType);
 });
 // localhost:9181/backend/certificateAmountGroupByCreateType
 
+// TODO 合作企业的通证分布
+confirmRouter.get('/certificateAmountGroupByCooperator', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByCooperator);
+});
+// localhost:9181/backend/certificateAmountGroupByCooperator
+
+// TODO 存证总数
+confirmRouter.get('/certificateAmountGroupByCooperator', async function(req, res) {
+    await UseMysql_c1(req, res, authDisplayGroup.handleCertificateAmountGroupByCooperator);
+});
+// localhost:9181/backend/certificateAmountGroupByCooperator
+
+
+/**************************/
+/****    数字版权证书    ****/
+/**************************/
 // 不同类别通证数量分布
-authRouter.get('/copyRightAmountGroupByCopyrightType', async function(req, res) {
+confirmRouter.get('/copyRightAmountGroupByCopyrightType', async function(req, res) {
     await UseMysql_c1(req, res, authDisplayGroup.handleCopyRightAmountGroupByCopyrightType);
 });
 // localhost:9181/backend/copyRightAmountGroupByCopyrightType
-
-
 /**************************/
 /****       监测维权     ****/
 /**************************/
-listenRouter.get('/TortCount', async function(req, res) {
+surveilanceRouter.get('/TortCount', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortCount);
 });
 // localhost:9181/backend/listen/TortCount
 
-listenRouter.get('/TortWorkCount', async function(req, res) {
+surveilanceRouter.get('/TortWorkCount', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortWorkCount);
 });
 // localhost:9181/backend/listen/TortWorkCount
 
-listenRouter.get('/PictureTortCountGroupByTortSite', async function(req, res) {
+surveilanceRouter.get('/PictureTortCountGroupByTortSite', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortCountGroupByTortSite, PICTURE_WORKTYPE);
 });
 // localhost:9181/backend/listen/PictureTortCountGroupByTortSite
 
-listenRouter.get('/MoiveTortCountGroupByTortSite', async function(req, res) {
+surveilanceRouter.get('/MoiveTortCountGroupByTortSite', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortCountGroupByTortSite, MOVIE_WORKTYPE);
 });
 // localhost:9181/backend/listen/MoiveTortCountGroupByTortSite
 
-listenRouter.get('/MusicTortCountGroupByTortSite', async function(req, res) {
+surveilanceRouter.get('/MusicTortCountGroupByTortSite', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortCountGroupByTortSite, MUSIC_WORKTYPE);
 });
 // localhost:9181/backend/listen/MusicTortCountGroupByTortSite
 
-listenRouter.get('/TortCountPictureGroupByTortSiteEXchange', async function(req, res) {
+surveilanceRouter.get('/TortCountPictureGroupByTortSiteEXchange', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortCountGroupByWorkTypeEXchange, PICTURE_WORKTYPE);
 });
 // localhost:9181/backend/listen/TortCountPictureGroupByTortSiteEXchange
 
-listenRouter.get('/TortCountMusicGroupByTortSiteEXchange', async function(req, res) {
+surveilanceRouter.get('/TortCountMusicGroupByTortSiteEXchange', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTortCountGroupByWorkTypeEXchange, MUSIC_WORKTYPE);
 });
 // localhost:9181/backend/listen/TortCountMusicGroupByTortSiteEXchange
 
-listenRouter.get('/Tort_AND_ClaimCountGroupByWorkType', async function(req, res) {
+surveilanceRouter.get('/Tort_AND_ClaimCountGroupByWorkType', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleTort_AND_ClaimCountGroupByWorkType);
 });
 // localhost:9181/backend/listen/Tort_AND_ClaimCountGroupByWorkType
 
-listenRouter.get('/LatestAlarm', async function(req, res) {
+surveilanceRouter.get('/LatestAlarm', async function(req, res) {
     await UseMysql_c2(req, res, listenDisplayGroup.handleLatestAlarm);
 });
 // localhost:9181/backend/listen/LatestAlarm
@@ -347,8 +395,8 @@ app.all('*', function(req, res, next) {
 });
 
 app.use('/', testRouter);
-app.use('/backend', authRouter);
-app.use('/backend/listen', listenRouter);
+app.use('/backend/confirmation', confirmRouter);
+app.use('/backend/surveilance', surveilanceRouter);
 
 
 /*----------启动http服务器----------*/
